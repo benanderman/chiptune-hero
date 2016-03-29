e610b67abf29edffb92071b651c45b2fc87b2605
Ben Anderman <bena@evidentpoint.com>
2016-03-29 14:51:16 -0400

    Changed indentation of two files to match the rest of the project

--- ChipTune Hero/ChipTune Hero/ChannelNode.swift
+++ ChipTune Hero/ChipTune Hero/ChannelNode.swift
@@ -9,93 +9,93 @@
 import SpriteKit
 
 class ChannelNode: SKSpriteNode {
-
-    let window: SKSpriteNode
+  
+  let window: SKSpriteNode
+  
+  override init(texture: SKTexture?, color: UIColor, size: CGSize) {
     
-    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
-        
-        self.window = SKSpriteNode(texture: nil, color: k.Color.Window, size: CGSizeMake(size.width, size.width))
-        super.init(texture: texture, color: color, size: size)
-        
-        self.window.position = CGPointMake(0, -size.height/2 + window.size.width/2)
-        self.window.zPosition = zPosition + 10
-        self.window.userInteractionEnabled = false
-        self.window.name = "Window"
-        
-        self.userInteractionEnabled = true
-        self.zPosition = 1
-        self.addChild(self.window)
-        
+    self.window = SKSpriteNode(texture: nil, color: k.Color.Window, size: CGSizeMake(size.width, size.width))
+    super.init(texture: texture, color: color, size: size)
+    
+    self.window.position = CGPointMake(0, -size.height/2 + window.size.width/2)
+    self.window.zPosition = zPosition + 10
+    self.window.userInteractionEnabled = false
+    self.window.name = "Window"
+    
+    self.userInteractionEnabled = true
+    self.zPosition = 1
+    self.addChild(self.window)
+    
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func startBlock(beats: Int) {
+    
+    let height: CGFloat = size.width * CGFloat(beats)
+    let block = NoteNode(color: UIColor(white: 1.0, alpha: 0.9), size: CGSizeMake(size.width, height))
+    block.position = CGPointMake(0, frame.midY + frame.width + height/2)
+    block.name = "Block"
+    block.zPosition = 5
+    
+    addChild(block)
+    
+    let x = k.Time.Fall/Double(arc4random_uniform(8))
+    block.runAction(SKAction.repeatActionForever(SKAction.sequence([SKAction.scaleBy(0.95, duration: x), SKAction.scaleBy(1.05, duration: x)])))
+    block.runAction(SKAction.moveToY(-frame.midY - frame.width, duration: k.Time.Fall)) {
+      block.removeFromParent()
+    }
+  }
+  
+  var startSuccess = false
+  
+  override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    
+    guard let block = childNodeWithName("Block") as? NoteNode else { return }
+    
+    if self.window.intersectsNode(block) {
+      startSuccess = true
+      return
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    func startBlock(beats: Int) {
+    block.color = UIColor.redColor()
+    startSuccess = false
+    
+  }
+  
+  override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    
+    if let block = childNodeWithName("Block") as? NoteNode {
+      if self.window.intersectsNode(block) && startSuccess {
+        let starSprite = SKSpriteNode(imageNamed: "Star")
         
-        let height: CGFloat = size.width * CGFloat(beats)
-        let block = NoteNode(color: UIColor(white: 1.0, alpha: 0.9), size: CGSizeMake(size.width, height))
-        block.position = CGPointMake(0, frame.midY + frame.width + height/2)
-        block.name = "Block"
-        block.zPosition = 5
+        starSprite.setScale(0.5)
+        starSprite.position = window.position
+        addChild(starSprite)
         
-        addChild(block)
-
-        let x = k.Time.Fall/Double(arc4random_uniform(8))
-        block.runAction(SKAction.repeatActionForever(SKAction.sequence([SKAction.scaleBy(0.95, duration: x), SKAction.scaleBy(1.05, duration: x)])))
-        block.runAction(SKAction.moveToY(-frame.midY - frame.width, duration: k.Time.Fall)) {
-            block.removeFromParent()
+        let emitter: SKEmitterNode = NSKeyedUnarchiver.unarchiveObjectWithFile(NSBundle.mainBundle().pathForResource("StarParticle", ofType: "sks")!) as! SKEmitterNode
+        emitter.particlePosition = CGPointMake(0, starSprite.size.height)
+        emitter.targetNode = self
+        starSprite.addChild(emitter)
+        
+        let doneAction = SKAction.fadeOutWithDuration(5)
+        let riseAction = SKAction.moveToY(900, duration: k.Time.Fall)
+        let spinAction = SKAction.rotateByAngle(CGFloat(3.14 * k.Time.Fall * 3), duration: k.Time.Fall * 3)
+        emitter.runAction(SKAction.fadeOutWithDuration(1))
+        starSprite.runAction(riseAction)
+        starSprite.runAction(spinAction)
+        
+        starSprite.runAction(doneAction) {
+          starSprite.removeFromParent()
         }
+        return
+      }
+      
+      block.color = UIColor.redColor()
+      
     }
- 
-    var startSuccess = false
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-
-        guard let block = childNodeWithName("Block") as? NoteNode else { return }
-        
-        if self.window.intersectsNode(block) {
-            startSuccess = true
-            return
-        }
-        
-        block.color = UIColor.redColor()
-        startSuccess = false
-
-    }
-    
-    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
-        if let block = childNodeWithName("Block") as? NoteNode {
-            if self.window.intersectsNode(block) && startSuccess {
-                let starSprite = SKSpriteNode(imageNamed: "Star")
-                
-                starSprite.setScale(0.5)
-                starSprite.position = window.position
-                addChild(starSprite)
-                
-                let emitter: SKEmitterNode = NSKeyedUnarchiver.unarchiveObjectWithFile(NSBundle.mainBundle().pathForResource("StarParticle", ofType: "sks")!) as! SKEmitterNode
-                emitter.particlePosition = CGPointMake(0, starSprite.size.height)
-                emitter.targetNode = self
-                starSprite.addChild(emitter)
-                
-                let doneAction = SKAction.fadeOutWithDuration(5)
-                let riseAction = SKAction.moveToY(900, duration: k.Time.Fall)
-                let spinAction = SKAction.rotateByAngle(CGFloat(3.14 * k.Time.Fall * 3), duration: k.Time.Fall * 3)
-                emitter.runAction(SKAction.fadeOutWithDuration(1))
-                starSprite.runAction(riseAction)
-                starSprite.runAction(spinAction)
-                
-                starSprite.runAction(doneAction) {
-                    starSprite.removeFromParent()
-                }
-                return
-            }
-            
-            block.color = UIColor.redColor()
-            
-        }
-    }
+  }
 }
 
--- ChipTune Hero/ChipTune Hero/GameScene.swift
+++ ChipTune Hero/ChipTune Hero/GameScene.swift
@@ -9,133 +9,133 @@
 import SpriteKit
 
 public struct k {
-    
-    public struct Name {
-        public static let Channel1 = "ChipTuneHero-Channel1"
-        public static let Channel2 = "ChipTuneHero-Channel2"
-        public static let Channel3 = "ChipTuneHero-Channel3"
-        public static let Channel4 = "ChipTuneHero-Channel4"
-    }
-    
-    public struct Color {
-        public static let Channel1 = UIColor.redColor()
-        public static let Channel2 = UIColor.blueColor()
-        public static let Channel3 = UIColor.purpleColor()
-        public static let Channel4 = UIColor.greenColor()
-        public static let Window   = UIColor(white: 0.3, alpha: 0.3)
-    }
-    
-    public struct Time {
-        public static let Fall = NSTimeInterval(1.25)
-    }
-    
+  
+  public struct Name {
+    public static let Channel1 = "ChipTuneHero-Channel1"
+    public static let Channel2 = "ChipTuneHero-Channel2"
+    public static let Channel3 = "ChipTuneHero-Channel3"
+    public static let Channel4 = "ChipTuneHero-Channel4"
+  }
+  
+  public struct Color {
+    public static let Channel1 = UIColor.redColor()
+    public static let Channel2 = UIColor.blueColor()
+    public static let Channel3 = UIColor.purpleColor()
+    public static let Channel4 = UIColor.greenColor()
+    public static let Window   = UIColor(white: 0.3, alpha: 0.3)
+  }
+  
+  public struct Time {
+    public static let Fall = NSTimeInterval(1.25)
+  }
+  
 }
 
 struct ChannelGenerator {
+  
+  let channel: ChannelNode
+  let beats: Int
+  let intervalCount: Int
+  let startOn: Bool
+  
+  init(channel: ChannelNode, beats: Int, intervalCount: Int, startOn: Bool) {
+    self.channel = channel
+    self.beats = beats
+    self.intervalCount = intervalCount
+    self.startOn = startOn
     
-    let channel: ChannelNode
-    let beats: Int
-    let intervalCount: Int
-    let startOn: Bool
+    isOn = startOn
+    interval = intervalCount
+  }
+  
+  var isOn: Bool
+  var interval: Int
+  
+  mutating func next() {
     
-    init(channel: ChannelNode, beats: Int, intervalCount: Int, startOn: Bool) {
-        self.channel = channel
-        self.beats = beats
-        self.intervalCount = intervalCount
-        self.startOn = startOn
-        
-        isOn = startOn
-        interval = intervalCount
+    if isOn {
+      channel.startBlock(beats)
     }
     
-    var isOn: Bool
-    var interval: Int
-
-    mutating func next() {
-
-        if isOn {
-            channel.startBlock(beats)
-        }
-        
-        interval -= 1
-        if interval < 0 {
-            isOn = !isOn
-            interval = intervalCount
-        }
-        
-        let hue = CGFloat(Double(arc4random_uniform(120) + 90)/Double(255))
-        let sat = CGFloat(Double(arc4random_uniform(120) + 100)/Double(255))
-        let bri = CGFloat(Double(arc4random_uniform(120) + 50)/Double(255))
-        
-        channel.color = UIColor(hue: hue, saturation: sat, brightness: bri, alpha: 1.0)
+    interval -= 1
+    if interval < 0 {
+      isOn = !isOn
+      interval = intervalCount
     }
+    
+    let hue = CGFloat(Double(arc4random_uniform(120) + 90)/Double(255))
+    let sat = CGFloat(Double(arc4random_uniform(120) + 100)/Double(255))
+    let bri = CGFloat(Double(arc4random_uniform(120) + 50)/Double(255))
+    
+    channel.color = UIColor(hue: hue, saturation: sat, brightness: bri, alpha: 1.0)
+  }
 }
 
 class GameScene: SKScene {
+  
+  let channel1: ChannelNode
+  let channel2: ChannelNode
+  let channel3: ChannelNode
+  let channel4: ChannelNode
+  
+  var timer1: NSTimer!
+  
+  override init(size: CGSize) {
     
-    let channel1: ChannelNode
-    let channel2: ChannelNode
-    let channel3: ChannelNode
-    let channel4: ChannelNode
+    let channelWidth = size.width/4
+    let channelSize = CGSizeMake(channelWidth - 2, size.height)
     
-    var timer1: NSTimer!
+    self.channel1 = ChannelNode(color: k.Color.Channel1, size: channelSize)
+    self.channel2 = ChannelNode(color: k.Color.Channel2, size: channelSize)
+    self.channel3 = ChannelNode(color: k.Color.Channel3, size: channelSize)
+    self.channel4 = ChannelNode(color: k.Color.Channel4, size: channelSize)
     
-    override init(size: CGSize) {
-        
-        let channelWidth = size.width/4
-        let channelSize = CGSizeMake(channelWidth - 2, size.height)
-        
-        self.channel1 = ChannelNode(color: k.Color.Channel1, size: channelSize)
-        self.channel2 = ChannelNode(color: k.Color.Channel2, size: channelSize)
-        self.channel3 = ChannelNode(color: k.Color.Channel3, size: channelSize)
-        self.channel4 = ChannelNode(color: k.Color.Channel4, size: channelSize)
-        
-        super.init(size: size)
-
-        self.channel1.position = CGPointMake(channelWidth * 0.5, self.frame.midY)
-        self.channel1.name = k.Name.Channel1
-        
-        self.channel2.position = CGPointMake(channelWidth * 1.5, self.frame.midY)
-        self.channel2.name = k.Name.Channel2
-        
-        self.channel3.position = CGPointMake(channelWidth * 2.5, self.frame.midY)
-        self.channel3.name = k.Name.Channel3
-        
-        self.channel4.position = CGPointMake(channelWidth * 3.5, self.frame.midY)
-        self.channel4.name = k.Name.Channel4
-        
-        addChild(self.channel1)
-        addChild(self.channel2)
-        addChild(self.channel3)
-        addChild(self.channel4)
-        
-
-        
-        var ch1 = ChannelGenerator(channel: channel1, beats: 1, intervalCount: 8, startOn: true)
-        var ch2 = ChannelGenerator(channel: channel2, beats: 2, intervalCount: 4, startOn: false)
-        var ch3 = ChannelGenerator(channel: channel3, beats: 1, intervalCount: 7, startOn: false)
-        var ch4 = ChannelGenerator(channel: channel4, beats: 2, intervalCount: 5, startOn: true)
-        
-        
-        NSTimer.every(k.Time.Fall/4.second) {
-            ch1.next()
-            ch2.next()
-            ch3.next()
-            ch4.next()
-        }        
+    super.init(size: size)
+    
+    self.channel1.position = CGPointMake(channelWidth * 0.5, self.frame.midY)
+    self.channel1.name = k.Name.Channel1
+    
+    self.channel2.position = CGPointMake(channelWidth * 1.5, self.frame.midY)
+    self.channel2.name = k.Name.Channel2
+    
+    self.channel3.position = CGPointMake(channelWidth * 2.5, self.frame.midY)
+    self.channel3.name = k.Name.Channel3
+    
+    self.channel4.position = CGPointMake(channelWidth * 3.5, self.frame.midY)
+    self.channel4.name = k.Name.Channel4
+    
+    addChild(self.channel1)
+    addChild(self.channel2)
+    addChild(self.channel3)
+    addChild(self.channel4)
+    
+    
+    
+    var ch1 = ChannelGenerator(channel: channel1, beats: 1, intervalCount: 8, startOn: true)
+    var ch2 = ChannelGenerator(channel: channel2, beats: 2, intervalCount: 4, startOn: false)
+    var ch3 = ChannelGenerator(channel: channel3, beats: 1, intervalCount: 7, startOn: false)
+    var ch4 = ChannelGenerator(channel: channel4, beats: 2, intervalCount: 5, startOn: true)
+    
+    
+    NSTimer.every(k.Time.Fall/4.second) {
+      ch1.next()
+      ch2.next()
+      ch3.next()
+      ch4.next()
     }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func sendChannel1Block() {
-        print("Hello")
-    }
-    
-
-    
-    func sendBlockOnChannel(channel: ChannelNode) {
-//        channel.startBlock()
-    }
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func sendChannel1Block() {
+    print("Hello")
+  }
+  
+  
+  
+  func sendBlockOnChannel(channel: ChannelNode) {
+    //        channel.startBlock()
+  }
 }
